0298ca90be0b5ad6d2ebdb4875f617dd08366bfa
Chinmay Panda <chinmay@openautonomyx.com>
2026-05-21 15:16:51 -0700

    Add benchmark registry

---
+++ agent_research/core/registry.py
@@ -0,0 +1,19 @@
+BENCHMARK_REGISTRY = {
+    "clearbench_mini": {
+        "domains": [
+            "customer_support",
+            "data_analysis",
+            "process_automation",
+            "software_development",
+            "compliance",
+            "multi_stakeholder",
+        ],
+        "metrics": [
+            "cost",
+            "latency",
+            "efficacy",
+            "assurance",
+            "reliability",
+        ],
+    }
+}
